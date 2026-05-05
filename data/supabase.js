3b92d67204d7d9e19c76713327428c79f12f71f0
Matheus Nunes de Souza <matheus.n.souza10@aluno.senai.br>
2026-05-05 14:22:09 -0300

    Update supabase.js

--- data/supabase.js
+++ data/supabase.js
@@ -4,7 +4,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-    throw new Error('SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não definido');
+    throw new Error('SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não definidos');
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
